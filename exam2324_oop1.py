57b5f94fa1eb8d56edc1a9db6276ad1abd8f5a19
Līga Migoļa <liga.migola@ventspils.edu.lv>
2025-02-12 15:17:27 +0200

    3

--- exam2324_oop1.py
+++ exam2324_oop1.py
@@ -1,1 +1,8 @@
-print ("hellow rolds")
+#4. daļa
+#1. uzdevums
+import csv
+with open('agenti.csv', newline='', encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+        print("Un vēl ir")
