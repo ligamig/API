f36ea7a4805d1b96f1854f24ab398058c02916fc
Līga Migoļa <liga.migola@ventspils.edu.lv>
2025-02-13 08:54:15 +0200

    4

--- exam2324_oop1.py
+++ exam2324_oop1.py
@@ -3,6 +3,19 @@
 import csv
 with open('agenti.csv', newline='', encoding='utf-8') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
-        print("Un vēl ir")
+    # for row in spamreader:
+    #     print(', '.join(row))
+    #     print("Un vēl ir")
+    dati = [row for row in spamreader]
+
+    iestades = [row for row in dati if row[0] in ['Izglītības iestāde', 'Valsts iestāde']]
+    for row in iestades:
+        print(iestades)
+
+    adrese = [row for row in iestades if 'Rīga' in row [2]]
+    for row in adrese:
+        print(adrese)
+
+    sorted = sorted(adrese, key=lambda x:x[1])
+    for row in sorted:
+        print(f"{row[1]} - {row[2]}")
